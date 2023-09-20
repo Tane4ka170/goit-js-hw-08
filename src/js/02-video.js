27592373228a452dda8f49e89d4c44876f0eb8b2
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-09-20 17:32:37 +0300

    task 2 (not final version)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerElement = document.querySelector('iframe')
+const player = new Player(playerElement)
+const localStorageKey = 'videoplayer-current-time';
+
+const saveCurrentTime = throttle(async () => {
+    try {
+        const currentTime = await player.getCurrentTime();
+        localStorage.setItem(localStorageKey, currentTime);
+    } catch (error) {
+        console.error('Failed to save current time:', error);
+    }
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime)
+
+const savedTime = localStorage.getItem(localStorageKey);
+if (savedTime !== null) {
+    player.setCurrentTime(savedTime);
+}
+player.play();
